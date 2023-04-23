1c7cbdf3d21310455a18503dd5bf0c6e3263b58f
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-23 13:59:18 +0530

    updating ./tf-module-alb

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lb_dns_name" {
-  value = aws_lb.main.dns_name
-}
-
-output "lb_listener_arn" {
-  value = aws_lb_listener.main.arn
-}
--- vars.tf
+++ vars.tf
@@ -1,7 +1,6 @@
 variable "env" {}
-variable "subnets" {}
+variable "private_subnets" {}
+variable "public_subnets" {}
 variable "name" {}
 variable "vpc_id" {}
 variable "vpc_cidr" {}
-variable "internal" {}
-variable "app_port_no" {}
