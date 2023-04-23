a28aa286968a39193ba73426ea4077e2095925aa
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-23 14:15:01 +0530

    updating ./tf-module-alb

--- internal.tf
+++ internal.tf
@@ -1,18 +1,18 @@
-resource "aws_lb" "internal" {
-  name               = "${var.env}-${var.name}-internal"
-  internal           = true
+resource "aws_lb" "main" {
+  name               = "${var.env}-${var.name}"
+  internal           = var.internal
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.internal.id]
-  subnets            = var.private_subnets
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnets
 
   tags = {
-    Environment = "production"
+    Environment = "${var.env}-${var.name}"
   }
 }
 
 resource "aws_security_group" "internal" {
-  name        = "${var.env}-${var.name}-internal-alb.sg"
-  description = "${var.env}-${var.name}-internal-alb.sg"
+  name        = "${var.env}-${var.name}-alb.sg"
+  description = "${var.env}-${var.name}-alb.sg"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -33,6 +33,6 @@ resource "aws_security_group" "internal" {
   }
 
   tags = {
-    Name = "${var.env}-${var.name}-internal-alb.sg"
+    Name = "${var.env}-${var.name}-alb.sg"
   }
 }
