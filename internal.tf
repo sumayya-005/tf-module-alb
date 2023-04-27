c41390d7da8cfadb9b12c13c08951e6d1318a788
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-27 11:48:05 +0530

    updating ./tf-module-alb

--- internal.tf
+++ internal.tf
@@ -1,8 +1,8 @@
 resource "aws_lb" "main" {
-  name               = "${var.env}-${var.name}"
+  name               = "${var.env}-${var.name}-internal"
   internal           = var.internal
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.main.id]
+  security_groups    = [aws_security_group.internal.id]
   subnets            = var.subnets
 
   tags = {
@@ -10,15 +10,15 @@ resource "aws_lb" "main" {
   }
 }
 
-resource "aws_security_group" "main"{
+resource "aws_security_group" "internal"{
   name        = "${var.env}-${var.name}-alb.sg"
   description = "${var.env}-${var.name}-alb.sg"
   vpc_id      = var.vpc_id
 
   ingress {
     description      = "HTTP"
-    from_port        = 22
-    to_port          = 22
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = [var.vpc_cidr]
   }
@@ -33,6 +33,6 @@ resource "aws_security_group" "main"{
   }
 
   tags = {
-    Name = "${var.env}-${var.name}-alb.sg"
+    Name = "${var.env}-${var.name}internal-alb.sg"
   }
 }
