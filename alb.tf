963b8997a07108d82b926d39e5c908d6a2767e20
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-04-28 11:14:32 +0530

    updating ./tf-module-alb

--- alb.tf
+++ alb.tf
@@ -1,53 +1,79 @@
-resource "aws_lb" "internal" {
-  name               = "${var.env}-${var.name}-internal"
-  internal           = var.internal
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.internal.id]
-  subnets            = var.subnets
+resource "aws_lb" "main" {
+name               = "${var.env}-${var.name}"
+internal           = var.internal
+load_balancer_type = "application"
+security_groups    = [aws_security_group.main.id]
+subnets            = var.subnets
 
-  tags = {
-    Environment = "${var.env}-${var.name}"
-  }
+tags = {
+Name = "${var.env}-${var.name}"
+}
 }
 
-resource "aws_security_group" "internal"{
-  name        = "${var.env}-${var.name}-alb.sg"
-  description = "${var.env}-${var.name}-alb.sg"
-  vpc_id      = var.vpc_id
+resource "aws_security_group" "main" {
+name        = "${var.env}-${var.name}-alb.sg"
+description = "${var.env}-${var.name}-alb.sg"
+vpc_id      = var.vpc_id
 
-  ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [var.vpc_cidr]
-  }
+ingress {
+description = "HTTP"
+from_port   = 80
+to_port     = 80
+protocol    = "tcp"
+cidr_blocks = var.internal ? [var.vpc_cidr] : ["0.0.0.0/0"]
+}
 
+ingress {
+description = "HTTPS"
+from_port   = 443
+to_port     = 443
+protocol    = "tcp"
+cidr_blocks = var.internal ? [var.vpc_cidr] : ["0.0.0.0/0"]
+}
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+egress {
+from_port        = 0
+to_port          = 0
+protocol         = "-1"
+cidr_blocks      = ["0.0.0.0/0"]
+ipv6_cidr_blocks = ["::/0"]
+}
 
-  tags = {
-    Name = "${var.env}-${var.name}-internal-alb.sg"
-  }
+tags = {
+Name = "${var.env}-${var.name}-alb.sg"
+}
 }
 
 resource "aws_lb_listener" "main" {
-  load_balancer_arn = "aws_lb_main.arn"
-  port              ="80"
-  protocol          = "HTTP"
-  default_action {
-    type = "fixed-response"
+count             = var.internal ? 1 : 0
+load_balancer_arn = aws_lb.main.arn
+port              = "80"
+protocol          = "HTTP"
 
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code = "200"
-    }
-  }
+default_action {
+type = "fixed-response"
+
+fixed_response {
+content_type = "text/plain"
+message_body = "Fixed response content"
+status_code  = "200"
+}
+}
+}
+
+resource "aws_lb_listener" "public-http" {
+count             = var.internal ? 0 : 1
+load_balancer_arn = aws_lb.main.arn
+port              = "80"
+protocol          = "HTTP"
+
+default_action {
+type = "redirect"
+
+redirect {
+port        = "443"
+protocol    = "HTTPS"
+status_code = "HTTP_301"
+}
+}
 }
