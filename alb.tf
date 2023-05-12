f333f1fcd6a4e87bcee053c11764cc6eec35796a
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-12 16:38:21 +0530

    updating ./tf-module-alb

--- alb.tf
+++ alb.tf
@@ -20,7 +20,7 @@ vpc_id      = var.vpc_id
    from_port   = 80
    to_port     = 80
    protocol    = "tcp"
-   cidr_blocks = [var.vpc_cidr]
+   cidr_blocks = var.internal ? [var.vpc_cidr] : ["0.0.0.0/0"]
 }
 
  egress {
