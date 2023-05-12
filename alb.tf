f201a001b286d46c43b4a7c082550aa01e96d0c9
sumayya-005 <107997976+sumayya-005@users.noreply.github.com>
2023-05-12 15:49:56 +0530

    updating ./tf-module-alb

--- alb.tf
+++ alb.tf
@@ -37,23 +37,23 @@ vpc_id      = var.vpc_id
 }
 }
 
-#resource "aws_lb_listener" "main" {
-#count             = var.internal ? 1 : 0
-#load_balancer_arn = aws_lb.main.arn
-#port              = "80"
-#protocol          = "HTTP"
-#
-#default_action {
-#type = "fixed-response"
-#
-#fixed_response {
-#content_type = "text/plain"
-#message_body = "Fixed response content"
-#status_code  = "200"
-#}
-#}
-#}
-#
+resource "aws_lb_listener" "main" {
+#  count             = var.internal ? 1 : 0
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+  fixed_response {
+   content_type = "text/plain"
+   message_body = "Fixed response content"
+   status_code  = "200"
+    }
+  }
+}
+
 #resource "aws_lb_listener" "public-http" {
 #count             = var.internal ? 0 : 1
 #load_balancer_arn = aws_lb.main.arn
